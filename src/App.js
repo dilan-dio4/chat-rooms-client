83db013daf1876909ccb91cb724a26c44a8401c3
dilan-dio4 <54545871+dilan-dio4@users.noreply.github.com>
2021-09-26 20:34:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Room({ currentRoom }) {
+    const [messageVal, setMessageVal] = useState("");
+    const [receivedMessages, setReceivedMessages] = useState([]);
+    const socketRef = useRef();
+
+    useEffect(() => {
+        setMessageVal("");
+        setReceivedMessages([]);
+
+        socketRef.current = new WebSocket("ws://localhost:8080");
+        const socket = socketRef.current;
+
+        socket.onopen = () => {
+            socket.send(JSON.stringify({
+                type: "ENTERED_ROOM",
+                payload: currentRoom
+            }));
+        }
+
+        socket.onmessage = (ev) => {
+            const data = JSON.parse(ev.data)
+            if (data.type === "RECEIVED_MESSAGE") {
+                setReceivedMessages(prev => [...prev, data.payload])
+            }
+        }
+    }, [currentRoom])
+    
+    const sendMessage = () => {
+        if (messageVal !== "") {
+            const socket = socketRef.current;
+            socket.send(JSON.stringify({
+                type: "SEND_MESSAGE",
+                payload: {
+                    room: currentRoom,
+                    message: messageVal
+                }
+            }));
+        }
+    }
+
+    return (
+        <div>
+            <p>In room: {currentRoom}</p>
+            {receivedMessages.map(msg => <p>{msg}</p>)}
+            <input value={messageVal} onChange={e => setMessageVal(e.target.value)} />
+            <button onClick={sendMessage}>send</button>
+        </div>
+    )
 }
 
-export default App;
+export default function App() {
+    const [currentRoom, setCurrentRoom] = useState(undefined);
+
+    return (
+        <div>
+            <button onClick={_ => setCurrentRoom(1)}>Room 1</button>
+            <button onClick={_ => setCurrentRoom(2)}>Room 2</button>
+            <button onClick={_ => setCurrentRoom(3)}>Room 3</button>
+            {currentRoom !== undefined && <Room currentRoom={currentRoom} />}
+        </div>
+    );
+}
